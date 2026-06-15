2e4a62ee6862e1582eb275bc95fac726ac802105
liked95 <101644745+liked95@users.noreply.github.com>
2026-06-15 17:00:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node OF the World Cup 2026. Brazil the champion!\n'
+  const msg = 'Hello Node OF the World Cup 2030. Brazil the champion!\n'
   res.end(msg);
 });
 
